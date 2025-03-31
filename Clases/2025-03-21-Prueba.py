8ab813ab294d91607c6cb3f04edbb1e1f59b2b6b
Bruno Fernandez <brunofernandez2008@gmail.com>
2025-03-31 19:58:35 -0300

    ClasesFolderCreated

---
+++ Clases/2025-03-21-Prueba.py
@@ -0,0 +1,154 @@
+print("Hola Mundo")
+
+# variable 
+# tipos datos primitivos int, float, bool, str, None
+
+# var_1 = 10
+# print(var_1, type(var_1))
+# var_1 = "Hola"
+# print(var_1, type(var_1))
+# var_1 = 10.7
+# print(var_1, type(var_1))
+# var_1 = True
+# print(var_1, type(var_1))
+
+# VAR = 123
+# print(VAR)
+
+# funciones de conversion int() float() str() bool()
+# numero = int(input("ingrese un numero: "))
+# print(numero * 2)
+
+# operaciones aritmeticas + - * / // % **
+
+# print(numero + 45)
+# print(numero - 45)
+# print(numero * 2)
+# print(numero / 2)
+# print(numero // 2)
+# print(numero % 2)
+# print(numero ** 3)
+
+#Control de flujo
+
+# Condicional, Bucle, Funciones 
+
+
+# if - if/else - anidados - case (>3.10)
+# operadores de control > < >= <= != ==
+
+num = 1
+
+# if num < 10:
+#     print("es menor")
+#     num = num * 5
+
+
+# if num < 10:
+#     print("es menor")
+#     num = num * 5
+# else:
+#     print("es mayor")
+
+# if num < 10:
+#     print("es menor")
+#     num = num * 5
+# else:
+#     if num == 10:
+#         print('es igual')
+#     else:
+#         print("es mayor")
+
+# if num < 10:
+#     print("es menor")
+#     num = num * 5
+# elif num == 10:
+#     print('es igual')
+# elif num > 10:
+#     print("es mayor")
+
+# case
+# opcion = input("ingrese una opcion (A, B, C) ")
+# match opcion:
+
+#     case 'A':
+#         print('opcion A')
+
+#     case 'B':
+#         print('opcion B')
+    
+#     case 'C':
+#         print('opcion C')
+
+#     case _:
+#         print("opcion no valida")
+
+# print("fin")
+
+# Bucles for | while
+
+
+num = 1
+
+# while num < 10:
+#     print(num)
+#     # num = num + 1
+#     num += 1
+
+# for i in range(1, 10):
+#     print(i)
+
+
+# estructuras de datos list | diccionario | tuplas 
+
+# lista_otra = ["hola", "mundo"]
+lista_num = []
+
+lista_num.append(0)
+lista_num.append(5)
+lista_num.append(7)
+lista_num.insert(1, 7)
+# lista_num.extend(lista_otra)
+# print("posicion", lista_num.index(5))
+# print(lista_num.count(77))
+# # print(lista_num.pop(2))
+# # print(lista_num.remove('hola'))
+# lista_num.sort()
+# lista_num[3] = 17
+# print(lista_num)
+
+# print(5 in lista_num)
+
+# key-value
+# diccionario = {
+#     32: 'Juan',
+#     14: 'Ana',
+#     44: 'Carlos',
+# } 
+# print(type(diccionario))
+# print(diccionario)
+
+# diccionario[44] = 'Julieta'
+# print(diccionario[44])
+# print(14 in diccionario)
+
+# print(diccionario.get(72))
+# print(diccionario.pop(14))
+# print(diccionario)
+
+# numeros = [1, 2, 3, 56, 45, 78]
+
+# for i in range(len(numeros)):
+#     print(numeros[i])  
+# for numero in numeros:
+#     print(numero)
+
+
+# print(type(numeros))
+# print(len(numeros))
+# print(numeros)
+# print(numeros[0], numeros[2], numeros[len(numeros)-1])
+
+
+# operadores logicos
+# VER STR
